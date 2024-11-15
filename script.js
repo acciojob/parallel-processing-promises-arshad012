654b0b1584ba50ef443079d34fcbea4b03c6012b
arshad012 <forarshad99@gmail.com>
2024-11-15 18:56:24 +0530

    created a submission

--- script.js
+++ script.js
@@ -7,3 +7,45 @@ const images = [
   { url: "https://picsum.photos/id/238/200/300" },
   { url: "https://picsum.photos/id/239/200/300" },
 ];
+
+const promise1 = new Promise((resolve, reject) => {
+	if(images[0]) {
+		return resolve(images[0]);
+	} else return reject(`Failed to load image's URL: ${images[0].url}`);
+})
+
+const promise2 = new Promise((resolve, reject) => {
+	if(images[1]) {
+		return resolve(images[1]);
+	} else return reject(`Failed to load image's URL: ${images[1].url}`);
+})
+
+const promise3 = new Promise((resolve, reject) => {
+	if(images[2]) {
+		return resolve(images[2]);
+	} else return reject(`Failed to load image's URL: ${images[2].url}`);
+})
+
+function downloadImages() {
+	Promise.all([promise1, promise2, promise3])
+	.then((res) => {
+		// console.log(res);
+		res.forEach((imageUrl, index) => {
+			console.log(imageUrl);
+			const image = document.createElement('img');
+			// image.style.width = '200px';
+			image.src = imageUrl;
+			image.alt = index+1;
+			output.append(image);
+		})
+	})
+	.catch((error) => {
+		console.log(error);
+	})
+}
+
+
+
+
+
+
